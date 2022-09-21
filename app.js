9d080a81e684515e860c1ca6203b0a3e869814fc
yoogukhyeon <rnrgus5897@gmail.com>
2022-09-21 14:00:55 +0900

    port 80

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
 })
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.listen(port, () => {
     console.log(`${port} 포트 포트로 이동중....`)
 })
